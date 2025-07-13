6f92bc7973cbcb9c62707f7f87413c6c74cae0b3
DoukheeWon <fain9301@yahoo.com>
2025-07-14 00:11:43 +0900

    feat: sum of proper divisor

--- chapter01/03-proper-divisor/main.h
+++ chapter01/03-proper-divisor/main.h
@@ -1,2 +1,22 @@
 #pragma once
 
+#include <cassert>
+#include <iostream>
+
+void ProperDivisor(int N) {
+    assert(3 < N);
+    assert(N <= 100);
+
+    int sum = 0;
+
+    for (int i = 1; i <= N; i++) {
+
+        if (N % i == 0) {
+            if (i == N) break;
+            if (i != 1) std::cout << " + ";
+            sum += i;
+            std::cout << i;
+        }
+    }
+    std::cout << " = " << sum << std::endl;
+}
--- chapter01/03-proper-divisor/test/test.cpp
+++ chapter01/03-proper-divisor/test/test.cpp
@@ -1,6 +1,98 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <sstream>
+#include <iostream>
+#include <string>
 
+#include "main.h"
+
+TEST(ProperDivisor, input_50) {
+    std::streambuf *originalBuffer = std::cout.rdbuf();
+
+    std::stringstream buffer;
+
+    std::cout.rdbuf(buffer.rdbuf());
+
+    ProperDivisor(50);
+
+    std::string output = buffer.str();
+
+    std::cout.rdbuf(originalBuffer);
+
+    EXPECT_EQ(output, "1 + 2 + 5 + 10 + 25 = 43\n");
+
+}
+
+
+TEST(ProperDivisor, input_10) {
+    std::streambuf *originalBuffer = std::cout.rdbuf();
+
+    std::stringstream buffer;
+
+    std::cout.rdbuf(buffer.rdbuf());
+
+    ProperDivisor(10);
+
+    std::string output = buffer.str();
+
+    std::cout.rdbuf(originalBuffer);
+
+    EXPECT_EQ(output, "1 + 2 + 5 = 8\n");
+
+}
+
+TEST(ProperDivisor, input_30) {
+    std::streambuf *originalBuffer = std::cout.rdbuf();
+
+    std::stringstream buffer;
+
+    std::cout.rdbuf(buffer.rdbuf());
+
+    ProperDivisor(30);
+
+    std::string output = buffer.str();
+
+    std::cout.rdbuf(originalBuffer);
+
+    EXPECT_EQ(output, "1 + 2 + 3 + 5 + 6 + 10 + 15 = 42\n");
+
+}
+
+
+TEST(ProperDivisor, input_70) {
+    std::streambuf *originalBuffer = std::cout.rdbuf();
+
+    std::stringstream buffer;
+
+    std::cout.rdbuf(buffer.rdbuf());
+
+    ProperDivisor(70);
+
+    std::string output = buffer.str();
+
+    std::cout.rdbuf(originalBuffer);
+
+    EXPECT_EQ(output, "1 + 2 + 5 + 7 + 10 + 14 + 35 = 74\n");
+
+}
+
+
+TEST(ProperDivisor, input_90) {
+    std::streambuf *originalBuffer = std::cout.rdbuf();
+
+    std::stringstream buffer;
+
+    std::cout.rdbuf(buffer.rdbuf());
+
+    ProperDivisor(90);
+
+    std::string output = buffer.str();
+
+    std::cout.rdbuf(originalBuffer);
+
+    EXPECT_EQ(output, "1 + 2 + 3 + 5 + 6 + 9 + 10 + 15 + 18 + 30 + 45 = 144\n");
+
+}
 
 
 int main(int argc, char **argv) {
