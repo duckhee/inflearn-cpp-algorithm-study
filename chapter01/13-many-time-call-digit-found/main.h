52a54bd1517ded4d147097537e6262cea0504078
DoukheeWon <fain9301@yahoo.com>
2025-07-26 18:13:21 +0900

    feat: set testing many time call digit found

--- chapter01/13-many-time-call-digit-found/main.h
+++ chapter01/13-many-time-call-digit-found/main.h
@@ -1,8 +1,25 @@
 #pragma once
 
 #include <cassert>
+#include <string>
+#include <iostream>
 
 
-int ManyTimeCallDigitFound(int number) {
+int ManyTimeCallDigitFound(std::string number) {
+    int i, digit, max = -1, res;
+    int ch[10] = {0,};
 
+    for (i = 0; number[i] != '\0'; i++) {
+        digit = number[i] - 48;
+        ch[digit]++;
+    }
+
+    for (i = 0; i < 10; i++) {
+        if (ch[i] >= max) {
+            max = ch[i];
+            res = i;
+        }
+    }
+
+    return res;
 }
--- chapter01/13-many-time-call-digit-found/test/test.cpp
+++ chapter01/13-many-time-call-digit-found/test/test.cpp
@@ -7,9 +7,122 @@
 #include <sstream>
 #include <chrono>
 
+std::string GetInput(const std::string &filename) {
+    std::ifstream inputFile(filename);
 
+    std::string inputNumber;
 
+    inputFile >> inputNumber;
 
+    return inputNumber;
+}
+
+int GetOutput(const std::string &filename) {
+    std::ifstream inputFile(filename);
+
+    int resultNumber = -1;
+
+    inputFile >> resultNumber;
+
+    if (resultNumber < 0) throw std::exception();
+
+    return resultNumber;
+}
+
+TEST(ManyTimeCallDigitFound, input_1) {
+    std::string inputNumber = GetInput("in1.txt");
+    auto start = std::chrono::system_clock::now();
+
+    int result = ManyTimeCallDigitFound(inputNumber);
+
+    auto end = std::chrono::system_clock::now();
+
+    auto executeTime = std::chrono::duration(end - start);
+
+    int expectResult = GetOutput("out1.txt");
+
+    EXPECT_EQ(result, expectResult)
+                        << "Failed to testing function result : " << result << ", expect result : " << expectResult
+                        << ", input number : " << inputNumber << ", execute time : " << executeTime.count() << "ms";
+
+    EXPECT_LT(executeTime.count(), 1000) << "실행 시간이 1초(1000ms)를 초과했습니다.";
+}
+
+TEST(ManyTimeCallDigitFound, input_2) {
+    std::string inputNumber = GetInput("in2.txt");
+    auto start = std::chrono::system_clock::now();
+
+    int result = ManyTimeCallDigitFound(inputNumber);
+
+    auto end = std::chrono::system_clock::now();
+
+    auto executeTime = std::chrono::duration(end - start);
+
+    int expectResult = GetOutput("out2.txt");
+
+    EXPECT_EQ(result, expectResult)
+                        << "Failed to testing function result : " << result << ", expect result : " << expectResult
+                        << ", input number : " << inputNumber << ", execute time : " << executeTime.count() << "ms";
+
+    EXPECT_LT(executeTime.count(), 1000) << "실행 시간이 1초(1000ms)를 초과했습니다.";
+}
+
+TEST(ManyTimeCallDigitFound, input_3) {
+    std::string inputNumber = GetInput("in3.txt");
+    auto start = std::chrono::system_clock::now();
+
+    int result = ManyTimeCallDigitFound(inputNumber);
+
+    auto end = std::chrono::system_clock::now();
+
+    auto executeTime = std::chrono::duration(end - start);
+
+    int expectResult = GetOutput("out3.txt");
+
+    EXPECT_EQ(result, expectResult)
+                        << "Failed to testing function result : " << result << ", expect result : " << expectResult
+                        << ", input number : " << inputNumber << ", execute time : " << executeTime.count() << "ms";
+
+    EXPECT_LT(executeTime.count(), 1000) << "실행 시간이 1초(1000ms)를 초과했습니다.";
+}
+
+TEST(ManyTimeCallDigitFound, input_4) {
+    std::string inputNumber = GetInput("in4.txt");
+    auto start = std::chrono::system_clock::now();
+
+    int result = ManyTimeCallDigitFound(inputNumber);
+
+    auto end = std::chrono::system_clock::now();
+
+    auto executeTime = std::chrono::duration(end - start);
+
+    int expectResult = GetOutput("out4.txt");
+
+    EXPECT_EQ(result, expectResult)
+                        << "Failed to testing function result : " << result << ", expect result : " << expectResult
+                        << ", input number : " << inputNumber << ", execute time : " << executeTime.count() << "ms";
+
+    EXPECT_LT(executeTime.count(), 1000) << "실행 시간이 1초(1000ms)를 초과했습니다.";
+}
+
+TEST(ManyTimeCallDigitFound, input_5) {
+    std::string inputNumber = GetInput("in5.txt");
+    auto start = std::chrono::system_clock::now();
+
+    int result = ManyTimeCallDigitFound(inputNumber);
+
+    auto end = std::chrono::system_clock::now();
+
+    auto executeTime = std::chrono::duration(end - start);
+
+    int expectResult = GetOutput("out5.txt");
+
+    EXPECT_EQ(result, expectResult)
+                        << "Failed to testing function result : " << result << ", expect result : " << expectResult
+                        << ", input number : " << inputNumber << ", execute time : " << executeTime.count() << "ms";;
+
+    EXPECT_LT(executeTime.count(), 1000) << "실행 시간이 1초(1000ms)를 초과했습니다.";
+}
 
 
 int main(int argc, char **argv) {
